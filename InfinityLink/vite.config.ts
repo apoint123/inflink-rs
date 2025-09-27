99fddc83d01f8ce3f29a837c06cc351f65be5915
apoint123 <108002475+apoint123@users.noreply.github.com>
2025-09-28 00:21:43 +0800

    build: 优化开发模式

--- InfinityLink/vite.config.ts
+++ InfinityLink/vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, type Plugin } from "vite";
 
-const buildRustPlugin = (): Plugin => {
+const buildRustPlugin = (mode: string): Plugin => {
 	const buildRust = () => {
 		console.log("构建rust程序...");
 		execSync("cargo build --release", {
@@ -16,7 +16,10 @@ const buildRustPlugin = (): Plugin => {
 	return {
 		name: "vite-plugin-build-rust",
 		buildStart: () => {
-			buildRust();
+			if (mode !== "development" || !buildRustPlugin.rustBuilt) {
+				buildRust();
+				buildRustPlugin.rustBuilt = true;
+			}
 		},
 		writeBundle: (options) => {
 			if (!options.dir) {
@@ -25,24 +28,42 @@ const buildRustPlugin = (): Plugin => {
 				);
 				return;
 			}
+			const outputDir = options.dir;
 
 			const manifestSrc = path.resolve(__dirname, "manifest.json");
-			const manifestDest = path.resolve(options.dir, "manifest.json");
+			const manifestDest = path.resolve(outputDir, "manifest.json");
 			fs.copyFileSync(manifestSrc, manifestDest);
 
 			const rustSrc = path.resolve(
 				__dirname,
 				"../target/release/smtc_handler.dll",
 			);
-
-			const rustDest = path.resolve(options.dir, "smtc_handler.dll");
+			const rustDest = path.resolve(outputDir, "smtc_handler.dll");
 			fs.copyFileSync(rustSrc, rustDest);
+
+			if (mode === "development") {
+				const devPluginDir = "C:/betterncm/plugins_dev/InfLink-rs";
+
+				fs.mkdirSync(devPluginDir, { recursive: true });
+
+				const filesInDist = fs.readdirSync(outputDir);
+
+				for (const file of filesInDist) {
+					if (file.endsWith(".dll")) {
+						continue;
+					}
+					const srcPath = path.join(outputDir, file);
+					const destPath = path.join(devPluginDir, file);
+					fs.copyFileSync(srcPath, destPath);
+				}
+			}
 		},
 	};
 };
+buildRustPlugin.rustBuilt = false;
 
 export default defineConfig(({ mode }) => ({
-	plugins: [react(), buildRustPlugin()],
+	plugins: [react(), buildRustPlugin(mode)],
 	define: {
 		"process.env": {},
 		DEBUG: mode === "development",
